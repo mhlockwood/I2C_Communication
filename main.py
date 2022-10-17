fa9526c4d513480277f8837e9a9d0a594af0458e
Margot Lockwood <mlockwood@ku.edu>
2022-10-17 00:38:36 -0800

    Adds two files.
    Adds config.yml, the configuration file. Adds the main file as well. Neither of these are currently in working shape.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #Hello, this is the main file
 
-from smbus import SMBus
+from dataclasses import dataclass
+#import smbus2 import SMBus
 from time import sleep
+import yaml
+from yaml.loader import SafeLoader
 
 #read in YAML file
+#called directly ~I think~
+""""
+if __name__ == '__main__':
+    stream = open("config.yml", 'r')
+"""
+with open('config.yml') as f:
+    variables = yaml.load(f, Loader=SafeLoader)
+    print(variables)
 
 #define variables from YAML file
 
